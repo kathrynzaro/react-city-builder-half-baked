2a46a9d44bc4e1b6b8e3c6a0d924f9b200d09424
kathrynzaro <zarokathryn@gmail.com>
2022-06-13 14:46:37 -0700

    slogans rendering

--- src/App.js
+++ src/App.js
@@ -39,8 +39,9 @@ function App() {
          
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
+        <SloganForm setSlogans={setSlogans} slogans={slogans} />
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-
+        <SloganList slogans={slogans} />
       </div>
     </div>
   );
